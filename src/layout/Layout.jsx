5d825ffdaa949f0ac75c4e2c75239703ad2c785b
a-khameneh <mt.d.abdollahian@gmail.com>
2024-09-24 16:51:00 +0330

    Layout is init

---
+++ src/layout/Layout.jsx
@@ -0,0 +1,20 @@
+
+function Layout({children}) {
+
+    return (
+
+        <>
+
+            <header></header>
+
+                {children}
+
+            <fouter></fouter>
+
+        </>
+
+    )
+
+}
+
+export default Layout;
